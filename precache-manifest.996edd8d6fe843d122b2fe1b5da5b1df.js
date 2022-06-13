9e08aa604bd8a35051e2bbebe44a0e4b2cb711fe
sahal akma <69509304+sahalakma@users.noreply.github.com>
2022-06-13 13:00:51 +0530

    Updates

---
+++ precache-manifest.996edd8d6fe843d122b2fe1b5da5b1df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37e9565c7819d65a490a807a2c5c035",
+    "url": "/food-items/index.html"
+  },
+  {
+    "revision": "06c40fd794194b3c6a91",
+    "url": "/food-items/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "317cceaa682868a51da0",
+    "url": "/food-items/static/js/2.cd2a891f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/food-items/static/js/2.cd2a891f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c40fd794194b3c6a91",
+    "url": "/food-items/static/js/main.a5af1b3f.chunk.js"
+  },
+  {
+    "revision": "398ec6f0ef913f521fee",
+    "url": "/food-items/static/js/runtime-main.aeb26168.js"
+  }
+]);
